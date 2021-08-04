fcfaa55eefacd2f2d638582c7d563be0b73b34fa
Dylan Awalt-Conley <web@dylan.ac>
2021-08-04 03:03:49 -0400

    added default includePaths for sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 const sassCompile = () => gulp.src('eleventy/css/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+        includePaths: [
+            'node_modules/normalize.css',
+            'node_modules/sass-color-helpers/stylesheets/'
+        ],
+        quietDeps: true
+    }).on('error', sass.logError))
     .pipe(gulp.dest('eleventy/_site/css'));
 
 const sassWatch = () => gulp.watch('eleventy/css/**/*.scss', sassCompile);
