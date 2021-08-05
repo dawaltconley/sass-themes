957b77cb31163d2c598d130972187e09e14e5f78
Dylan Awalt-Conley <web@dylan.ac>
2021-08-05 17:04:28 -0400

    updated color-helpers path; only need to provide the install location for sass to process

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const sass = require('gulp-sass')(require('sass'));
 const sassCompile = () => gulp.src('eleventy/css/**/*.scss')
     .pipe(sass({
         includePaths: [
-            'node_modules/normalize.css',
-            'node_modules/sass-color-helpers/stylesheets/'
+            'node_modules',
+            'node_modules/normalize.css'
         ],
         quietDeps: true
     }).on('error', sass.logError))
