bef157f6ae09201852e0f9de1e1f63cc80e5f275
Dylan Awalt-Conley <web@dylan.ac>
2022-07-17 16:38:28 -0400

    style: prettify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,44 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const fsp = require('fs').promises;
-const assert = require('assert').strict;
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const fsp = require('fs').promises
+const assert = require('assert').strict
 
-const sassCompile = () => gulp.src('eleventy/css/**/*.scss')
-    .pipe(sass({
+const sassCompile = () =>
+  gulp
+    .src('eleventy/css/**/*.scss')
+    .pipe(
+      sass({
         includePaths: [
-            'node_modules',
-            'node_modules/@dawaltconley',
-            'node_modules/normalize.css'
+          'node_modules',
+          'node_modules/@dawaltconley',
+          'node_modules/normalize.css',
         ],
-        quietDeps: true
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('eleventy/_site/css'));
+        quietDeps: true,
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest('eleventy/_site/css'))
 
-const sassWatch = () => gulp.watch('eleventy/css/**/*.scss', sassCompile);
+const sassWatch = () => gulp.watch('eleventy/css/**/*.scss', sassCompile)
 
 const sassCompare = cb => {
-    let buf = [];
-    let expected = fsp.readFile('test/main.css');
-    sassCompile()
-        .on('data', data => buf.push(data.contents))
-        .on('end', async () => {
-            const output = Buffer.concat(buf).toString();
-            expected = (await expected).toString();
-            try {
-                assert.deepEqual(output, expected);
-                cb();
-            } catch (error) {
-                cb(error);
-            }
-        });
-};
+  let buf = []
+  let expected = fsp.readFile('test/main.css')
+  sassCompile()
+    .on('data', data => buf.push(data.contents))
+    .on('end', async () => {
+      const output = Buffer.concat(buf).toString()
+      expected = (await expected).toString()
+      try {
+        assert.deepEqual(output, expected)
+        cb()
+      } catch (error) {
+        cb(error)
+      }
+    })
+}
 
-exports.build = sassCompile;
+exports.build = sassCompile
 
-exports.serve = gulp.series(sassCompile, sassWatch);
+exports.serve = gulp.series(sassCompile, sassWatch)
 
-exports.test = sassCompare;
+exports.test = sassCompare
